d59916eeaef43d8eed900fdfbf16443da6caa8ff
young <liyang_email@163.com>
2024-09-28 13:13:02 +0800

    feat(api): service template logic add

--- src/lib/handle_template_data/service.ts
+++ src/lib/handle_template_data/service.ts
@@ -24,6 +24,7 @@ export function handleServiceTemplateData(
 	openAPI: OpenAPI3,
 ): ServiceTplData {
 	const apiData: ApiDataItem[] = [];
+	const paths: any[] = [];
 	return {
 		requestLibPath,
 		apiData,
