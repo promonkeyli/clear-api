e886d6dda9c4725418a36f70d04f78988bd49bab
young <liyang_email@163.com>
2024-09-28 13:14:33 +0800

    feta(api): update files

--- index.test.js
+++ index.test.js
@@ -1,7 +1,8 @@
-import { generateAPI } from "./index.js";
+import { generateAPI } from "./dist/index.js";
 
-const docUrl = 'https://petstore.swagger.io/v2/swagger.json'
-const requestLibPath = "import request from '@/utils/http'"
-const outDir = 'service'
+const docUrl = "https://petstore.swagger.io/v2/swagger.json";
+// const docUrl = "https://promonkeyli.top:8080/swagger/doc.json";
+const requestLibPath = "import request from '@/utils/http'";
+const outDir = "";
 
-generateAPI({docUrl, requestLibPath, outDir})
+generateAPI({ docUrl, requestLibPath, outDir });
